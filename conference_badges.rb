0b17864cf8b66941c051d3e1e8693ae1ab1f905f
MichaelAbe <michael.k.abe@gmail.com>
2020-11-11 11:35:11 -0500

    commit tree 95925bbaad87a3628676275852786f91cd43474e

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    new = []
+    array.each do |name|
+        new.push "Hello, my name is #{name}."
+    end
+    new
+end
+
+def assign_rooms(name)
+    new = []  
+    name.each_with_index do |name, index|
+      new.push "Hello, #{name}! You'll be assigned to room #{index +1}!"
+  end
+ new
+end
+#takes the list of speakers and assigns each speaker to a room. Make sure that each room only has one speaker.
+#"Hello, _____! You'll be assigned to room _____!"
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |new|
+      puts new
+    end
+    assign_rooms(attendees).each do |new|
+      puts new
+    end
+end
+
